b07e40a440a86e4884c0ae59bb80a0015d821316
Md Anas <97391971+MdAnas6757@users.noreply.github.com>
2024-02-01 20:25:28 +0530

    change build

---
+++ client/build/precache-manifest.9751aa6a38e45b6ca7876e754c8c2388.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9828e2c10986682caf3ed7d15a97e7d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca2fd70f77e342cbdc26",
+    "url": "/static/css/main.8249ba95.chunk.css"
+  },
+  {
+    "revision": "d779e14bfe2dcd80fca9",
+    "url": "/static/js/2.f19c1f85.chunk.js"
+  },
+  {
+    "revision": "ca2fd70f77e342cbdc26",
+    "url": "/static/js/main.2ae194da.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e342bbedde341fc8400c29866f37940b",
+    "url": "/static/media/memories-Logo.e342bbed.png"
+  },
+  {
+    "revision": "fcbcfa5ad4437d1d7d34bbbbeec30305",
+    "url": "/static/media/memories-Text.fcbcfa5a.png"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9762871b01c2a4cf32a4c2871f895c1f.js"
+  "/precache-manifest.9751aa6a38e45b6ca7876e754c8c2388.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
