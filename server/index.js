963404489464337e059797e624c80f87d17e272d
Md Anas <97391971+MdAnas6757@users.noreply.github.com>
2024-02-02 00:10:49 +0530

    update server

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const app=express();
 app.use(bodyParser.json({limit:"30mb",extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: 'https://memories-server-h2c0.onrender.com/'
 })); 
 app.use('/posts',postRoutes);
 app.use('/user',userRoutes);
