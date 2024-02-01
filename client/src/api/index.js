3053978695c90d057efbac9e94d686a27780e6df
Md Anas <97391971+MdAnas6757@users.noreply.github.com>
2024-02-01 23:28:22 +0530

    update api.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API=axios.create({baseURL:'http://localhost:5000'})
+const API=axios.create({baseURL:'https://memories-server-h2c0.onrender.com/' || 'http://localhost:5000'})
 API.interceptors.request.use((req)=>{
     if(localStorage.getItem('profile'))
     {
