d10cac1b58bb235e35ee5babbd3d339acba6766c
Md Anas <97391971+MdAnas6757@users.noreply.github.com>
2024-02-10 00:55:09 +0530

    previos commit

---
+++ client/build/precache-manifest.8b1c7b212fd782f49639c2c9646789b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4920c16e823c482e52f363c2e2145b25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8b384bf837ef712e333",
+    "url": "/static/css/main.8249ba95.chunk.css"
+  },
+  {
+    "revision": "d779e14bfe2dcd80fca9",
+    "url": "/static/js/2.f19c1f85.chunk.js"
+  },
+  {
+    "revision": "c8b384bf837ef712e333",
+    "url": "/static/js/main.7c2af82b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e342bbedde341fc8400c29866f37940b",
+    "url": "/static/media/memories-Logo.e342bbed.png"
+  },
+  {
+    "revision": "fcbcfa5ad4437d1d7d34bbbbeec30305",
+    "url": "/static/media/memories-Text.fcbcfa5a.png"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a06fb48257e875c223d0be3873bc7fb2.js"
+  "/precache-manifest.8b1c7b212fd782f49639c2c9646789b9.js"
 );
 
 self.addEventListener('message', (event) => {
