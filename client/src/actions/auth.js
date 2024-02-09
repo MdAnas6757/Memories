529f9c2f751703d63e44d095a6f0597c440a9481
Ayush-Kumar0 <ayushkumar27053@gmail.com>
2024-02-10 00:38:41 +0530

    reverted

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,26 +1,27 @@
 import { AUTH } from '../constants/actionTypes';
-import {message} from "antd";
+import { message } from "antd";
 import * as api from '../api/index.js';
-export const signin=(formData,navigate)=> async(dispatch)=>{
+export const signin = (formData, navigate) => async (dispatch) => {
     try {
-        const {data}=await api.signIn(formData);
-        
-        dispatch({type:AUTH,data});
+        const { data } = await api.signIn(formData);
+
+        dispatch({ type: AUTH, data });
         navigate('/posts');
     } catch (error) {
         message.error(error.message);
-        
+
     }
 }
-export const signup=(formData,navigate)=> async(dispatch)=>{
+export const signup = (formData, navigate) => async (dispatch) => {
     try {
-        const {data}=await api.signUp(formData);
-        dispatch({type:AUTH,data});
+        let x;
+        const { data } = x = await api.signUp(formData);
+        console.log(x);
+        dispatch({ type: AUTH, data });
         navigate('/posts');
-    } catch (error) 
-    {
-       
+    } catch (error) {
+        console.log(error);
         message.error(error.message);
-        
+
     }
 }
--- server/index.js
+++ server/index.js
@@ -10,22 +10,23 @@ const __dirname = path.dirname(new URL(import.meta.url).pathname);
 dotenv.config();
 //database coonection
 connectDB();
-const app=express();
+const app = express();
 
-app.use(bodyParser.json({limit:"30mb",extended:true}));
-app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors({
-    origin: 'https://memories-server-h2c0.onrender.com/'
-})); 
-app.use('/posts',postRoutes);
-app.use('/user',userRoutes);
+    origin: 'https://memories-server-h2c0.onrender.com/',
+    credentials: true,
+}));
+app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get('*', function(req, res) {
+app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-const port=process.env.PORT||5000
+const port = process.env.PORT || 5000
 //lisen port
-app.listen(port,()=>{
-    console.log(`Server Running in ${process.env.MONGO_URI} Mode on port ${process.env.PORT}`.bgCyan.white)
+app.listen(port, () => {
+    console.log(`Server Running!!`.bgCyan.white)
 })
